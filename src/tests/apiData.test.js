26a6698d72415a4440fd2e545fc0012a5b8824b6
finteger <nashassociatesinc@gmail.com>
2025-09-22 18:56:34 -0600

    Update

--- src/tests/apiData.test.js
+++ src/tests/apiData.test.js
@@ -1,5 +1,5 @@
 //import the function to be tested
-const { fetchData } = require('./functions/apiData.js');
+const { fetchData } = require('../functions/apiData.js');
 
 //Mock this global fetch function
 global.fetch = jest.fn();
@@ -29,5 +29,4 @@ describe('fetchData',  () => {
         expect(data).toEqual(mockResponse);
         expect(fetch).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/todos/1');
     });
-
 });
