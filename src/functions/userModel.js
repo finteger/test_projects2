c95fbcc71d3205433567bf1377b0f7d540024ba9
finteger <nashassociatesinc@gmail.com>
2025-09-29 18:55:19 -0600

    Update

--- src/functions/userModel.js
+++ src/functions/userModel.js
@@ -1,12 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //Creating a user schema
 const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true},
-    age: { type: Number, min: 7, max: 120 }
+  firstName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  age: { type: Number, min: 7, max: 120 },
 });
 
-const User = mongoose.model('Users', userSchema);
+const User = mongoose.model("Users", userSchema);
 
+//Controller function for user creation object
+exports.createUser = async (firstName, email, password, age) => {
+  try {
+    const user = new User({firstName, email, password, age });
+    await user.save();
+  } catch (error) {
+    throw new Error('Something went wrong with creating a new user.');
+  }
+};
+
+//not needed: module.exports = {};
