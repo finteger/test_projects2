30778202bf131ce89cf7cf3af6a2a92a4777b80c
finteger <nashassociatesinc@gmail.com>
2025-10-07 18:48:05 -0600

    Update

--- src/functions/userModel.js
+++ src/functions/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  age: { type: Number, min: 7, max: 120 },
+  age: { type: Number},
 });
 
 const User = mongoose.model("User", userSchema);
