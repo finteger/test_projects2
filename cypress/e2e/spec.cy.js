13c4e006ca4868a37034fec576e1c336d3cb4ce5
finteger <nashassociatesinc@gmail.com>
2025-10-16 17:58:33 -0600

    Update

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,14 @@
+describe('template spec', () => {
+  it('Gets, types, and asserts', () => {
+
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    cy.url().should('include', '/commands/actions')
+
+    cy.get('.action-email').type('fakeemail@gmail.com')
+
+    cy.get('.action-email').should('have.value', 'fakeemail@gmail.com')
+  })
+})
